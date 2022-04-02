677744463275cab54ceb65108822acef36a195ff
Brandon Tor <brandon.tor@hotmail.com>
2022-04-01 20:57:21 -0400

    firebase config success

---
+++ components/firebaseconfig.js
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+    
+    apiKey: "AIzaSyDk4XhJ3xybfGhyYnWziDhdcMKCrEUbwI4",
+    authDomain: "btayoan-5757a.firebaseapp.com",
+    projectId: "btayoan-5757a",
+    storageBucket: "btayoan-5757a.appspot.com",
+    messagingSenderId: "979861727460",
+    appId: "1:979861727460:web:66240af2c9b3b7d76024f1",
+    measurementId: "G-4FJHPY2NZF"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
