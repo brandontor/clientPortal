8dfc988b744d9075e41c9fc7b193b73436c82545
Brandon Tor <brandon.tor@hotmail.com>
2022-04-03 11:33:35 -0400

    added forum post on and qc

---
+++ components/forum/ForumPostQc.js
@@ -0,0 +1,29 @@
+import { TextField } from '@mui/material'
+import React from 'react'
+import styles from './ForumPostQc.module.css'
+
+function ForumPostQc() {
+
+  return (
+    <div className={styles.container}>
+        <div className={styles.header}>
+            <p>Quebec - Skin - ForumPost</p>
+        </div>
+        <div className={styles.body}>
+            <div className={styles.title}>
+                <p>Hello World</p>
+            </div>
+            <div className={styles.question}>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
+            <div className={styles.answer}>
+                <textarea placeholder='Comment'></textarea>
+                <button>Submit</button>
+            </div>
+        </div>
+
+    </div>
+    )
+}
+
+export default ForumPostQc
