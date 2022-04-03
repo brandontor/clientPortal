f8b1e83ee0fe34b05347bc17f31e5fe1b745844b
Brandon Tor <brandon.tor@hotmail.com>
2022-04-03 12:49:07 -0400

    refactored for qc forum post

--- components/forum/ForumPostQc.js
+++ components/forum/ForumPostQc.js
@@ -1,27 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
+import CommentCard from './comments/CommentCard'
 import styles from './ForumPostQc.module.css'
 
 function ForumPostQc() {
 
-  return (
-    <div className={styles.container}>
-        <div className={styles.header}>
-            <p>Quebec - Skin - ForumPost</p>
-        </div>
-        <div className={styles.body}>
-            <div className={styles.title}>
-                <p>Hello World</p>
-            </div>
-            <div className={styles.question}>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-            </div>
-            <div className={styles.answer}>
-                <textarea placeholder='Comment'></textarea>
-                <button>Submit</button>
-            </div>
-        </div>
 
-    </div>
+    const [showComments, setShowComments] = useState(false)
+    const commentsHandler = () => {
+        if(showComments) {
+            setShowComments(false)
+            return
+        }
+        setShowComments(true)
+    }
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <p>Ontario - Skin - ForumPost</p>
+            </div>
+            <div className={styles.body}>
+                <div className={styles.title}>
+                    <p>Hello World</p>
+                </div>
+                <div className={styles.question}>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                </div>
+                <div className={styles.answer}>
+                    <textarea placeholder='Comment'></textarea>
+                    <button>Submit</button>
+                </div>
+                <div className={styles.comments}>
+                    <button onClick={commentsHandler}>Show Comments</button>
+                    {showComments && 
+                    <div className={styles.commentsFeed}>
+                        <ul>
+                            <CommentCard></CommentCard>
+                            <CommentCard></CommentCard>
+                        </ul>
+                    </div>
+                    }
+                </div>
+            </div>
+        </div>
     )
 }
 
